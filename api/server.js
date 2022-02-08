3eea8a350f060c25f3f193c3d9b345ebca26c65d
John S <johnc.sime@gmail.com>
2022-02-08 17:37:29 -0500

    Got the post endpoints working in post-router.js

--- api/server.js
+++ api/server.js
@@ -3,10 +3,8 @@
 const express = require('express');
 const postRouter = require('./posts/posts-router');
 const server = express();
-
-server.use('/api/posts', postRouter)
-
 server.use(express.json());
+server.use('/api/posts', postRouter)
 
 server.use('*', (req, res)=>{
     res.status(404).json({
